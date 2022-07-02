66793f3e030061688b8f107bc4fe9e541ec1d272
esraa-abdelmaksoud <esraa.e.abdelmaksoud@gmail.com>
2022-07-02 23:14:56 +0200

    pushing-all

--- I4G002040ELX/links/views.py
+++ I4G002040ELX/links/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Link
+from .serizliers import LinkSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
 
 # Create your views here.
+class LinkListAPI(generics.ListAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class LinkCreateApi(generics.CreateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class LinkDetailApi(generics.RetrieveAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class LinkUpdateApi(generics.UpdateAPIView):
+    queryset = Link.object.filter(active=True)
+    serializer_class = LinkSerializer
+
+class LinkDeleteApi(generics.DestroyAPIView):
+    queryset= Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
